730255cf29a0ff31bbf60d1f9db8ca7b8e9e37a2
Yaroslav Klym <92799954+KOlegaBB@users.noreply.github.com>
2021-12-15 13:10:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,34 @@ def read_placement():
     """
     return input().split()
 
+def shot(board):
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", ]
+    print("Choose coordinate")
+    for line in board:
+        print(line)
+    coordinate = input(">>>")
+    while True:
+        if coordinate[0] in letters:
+            column = letters.index(coordinate[0]) + 1
+            break
+        else:
+            print("Your coordinate is not exist")
+    while True:
+        if 0 <= int(coordinate[1]) <= 9:
+            row = letters.index(coordinate[0]) + 1
+            break
+        else:
+            print("Your coordinate is not exist")
+    if board[row][column] == "*":
+        print("You hit")
+        board[row][column] = "X"
+    elif board[row][column] == "X" or board[row][column] == ".":
+        print("You already shot there")
+    else:
+        print("You missed")
+        board[row][column] = "."
+    return board
+
 
 if __name__ == "__main__":
     field1,field2 = [([0] for i in range(11)) for i in range(11)], [([0] for i in range(11)) for i in range(11)]
