1d156e24e4e830e4ba49fa54070b763028006572
Anastasiia Beheni <91615487+beheni@users.noreply.github.com>
2021-12-15 13:10:13 +0200

    added function for field visual

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
-"""this is our main"""
+"""
+this is our main
+ship is a tuple: ((x,y), length, h(for horizontal)/v(for vertical))
+"""
+
+def help():
+    print('This is a battleship game for two players on 10 on 10 field')
+    print('Wait for your turn and then enter coordinates and the direction (v for vertical, h for horizontal')
+    print('Write just (x,y) for shot')
+    print('Example for ship: "b4 v"')
+
+def read_placement():
+    """
+    Reads and checks user input
+    Returns: a coordinate and v/h
+    Example: ('b4',v)
+    """
+    return input().split()
+
+def field_visual(list: list) -> str:
+    field = ''
+    for i in range(len(list)):
+        for j in list[i]:
+            field += str(j)
+        field+='\n'
+    return field
+
+def place(field: list, coordinates: tuple):
+    # coordinates = ("b4", "v") or ("b2", "h")
+    return field_visual(field)
 
 
 if __name__ == "__main__":
-    pass
+    field1,field2 = [([0] for i in range(11)) for i in range(11)], [([0] for i in range(11)) for i in range(11)]
+    ships1,ships2 = ['*', '**', '***'], ['*', '**', '***']
+
+    help()
+    for ship in ships1:
+        print(f'Next ship is {ship}')
+        coordinates = read_placement()
+        # field1 = place(field1, coordinates)
+        # print(field_visual(field1))
+    for ship in ships2:
+        print(f'Next ship is {ship}')
+        coordinates = read_placement()
+        print(read_placement())
+        # field1 = place(field1, coordinates)
+        # print(field_visual(field2))
